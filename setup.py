4af879522edb231b93846c1e87d4cac32fda5d9d
Bryant Moscon <bryant@curata.com>
2017-10-11 18:38:40 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name="articleparse",
+    version="0.1.0",
+    author="Bryant Moscon",
+    author_email="bmoscon@gmail.com",
+    description=("Heuristic text extraction from news articles"),
+    license="GPL",
+    keywords=["text extraction", ],
+    url="https://github.com/bmoscon/articleparse",
+    packages=find_packages(exclude=['tests']),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
